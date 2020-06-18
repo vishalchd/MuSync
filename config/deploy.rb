6135bc4cb913b5ab1c00e93dc86d2e5df2bf5287
Vishal <vishal.chd@gmail.com>
2020-06-18 18:27:16 +0530

    uncommented missing deployment files

--- config/deploy.rb
+++ config/deploy.rb
@@ -54,9 +54,9 @@ task :deploy do
     invoke :'git:clone'
     invoke :'deploy:link_shared_paths'
     invoke :'bundle:install'
-    #invoke :'rails:db_migrate'
-    #invoke :'rails:assets_precompile'
-    #invoke :'deploy:cleanup'
+    invoke :'rails:db_migrate'
+    invoke :'rails:assets_precompile'
+    invoke :'deploy:cleanup'
 
     on :launch do
       in_path(fetch(:current_path)) do
